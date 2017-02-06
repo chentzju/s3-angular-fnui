5c375927d5e5bf687e1b9d1e65fed27b3a60e947
陈涛 <chent.zju@gmail.com>
2017-02-06 16:10:02 +0800

    更改了打包配置

--- gulpfile.js
+++ gulpfile.js
@@ -34,6 +34,14 @@ gulp.task("assets",function(){
     gulp.src('node_modules/fnui/dist/css/*').pipe(gulp.dest('dist/assets/styles'));
 });
 
+
+//copy files
+gulp.task('copy',function(){
+    gulp.src('src/app/templates/*').pipe(gulp.dest('dist/templates'));
+    gulp.src('src/index.html').pipe(gulp.dest('dist'));
+    gulp.src('src/404.html').pipe(gulp.dest('dist'));
+});
+
 //build app
 gulp.task('image',function(){
     gulp.src('src/app/images/*')
@@ -41,18 +49,14 @@ gulp.task('image',function(){
         .pipe(gulp.dest('dist/images'))
 });
 
-gulp.task('copy',function(){
-    gulp.src('src/app/templates/*').pipe(gulp.dest('dist/templates'));
-    gulp.src('src/index.html').pipe(gulp.dest('dist'));
-    gulp.src('src/404.html').pipe(gulp.dest('dist'));
-});
-
+//views changed
 gulp.task('views',function(){
     config.modules.forEach(function (item) {
         fse.copySync(item.path, 'dist/views/'+item.name);
     })
 });
 
+//
 gulp.task('client',function(){
     gulp.src(['src/app/app.js','src/app/components/*'])
         .pipe(concat('app.js'))
@@ -64,21 +68,55 @@ gulp.task('client',function(){
         .pipe(gulp.dest('dist/styles'));
 });
 
+
+
 gulp.task('clean', function () {
     fse.emptyDirSync('dist');
 });
 
-
-
 gulp.task('build',['clean', 'assets', 'copy', 'image','views','client']);
-
 gulp.task('default',['build']);
 
 
 
+
+// dev enviroment
+gulp.task("assets-dev",function(){
+    gulp.src([
+        'bower_components/jquery/dist/jquery.js',
+        'node_modules/fnui/dist/js/fnui.js',
+        'bower_components/iscroll/build/iscroll-probe.js'
+    ])
+        .pipe(concat("vendor.js"))
+        .pipe(gulp.dest('dist/assets/js'));
+
+    gulp.src([
+        'bower_components/angular/angular.js',
+        'bower_components/angular-ui-router/release/angular-ui-router.min.js',
+        'bower_components/angular-animate/angular-animate.js',
+        'bower_components/oclazyload/dist/ocLazyLoad.js'
+    ])
+        .pipe(concat('angular.min.js'))
+        .pipe(gulp.dest('dist/assets/js'));
+
+    gulp.src('node_modules/fnui/dist/fonts/*').pipe(gulp.dest('dist/assets/fonts'));
+    gulp.src('node_modules/fnui/dist/css/*').pipe(gulp.dest('dist/assets/styles'));
+});
+
+gulp.task('client-dev',function(){
+    gulp.src(['src/app/app.js','src/app/components/*'])
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('dist/scripts'));
+
+    gulp.src('src/app/*.css')
+        .pipe(concat('app.css'))
+        .pipe(gulp.dest('dist/styles'));
+});
+gulp.task('dev',['clean', 'assets-dev', 'copy', 'image','views','client-dev']);
+
 gulp.task('watch', function () {
     gulp.watch(['src/index.html','src/app/templates/*'], ['copy','reload']);
-    gulp.watch(['src/app/components/*','src/app/*.css'], ['client','reload']);
+    gulp.watch(['src/app/*.js','src/app/components/*','src/app/*.css'], ['client-dev','reload']);
     gulp.watch(['src/app/views/**/*.html','src/app/views/**/**/*'], ['views','reload']);
     gulp.watch(['src/index.html'], ['copy','reload']);
     gulp.watch(['src/app/images/*'],['image','reload']);
@@ -87,9 +125,7 @@ gulp.task('watch', function () {
 gulp.task('reload',function () {
     gulp.src('dist').pipe(connect.reload())
 });
-
-
-gulp.task('connect',['build'],function(){
+gulp.task('connect',['dev'],function(){
     connect.server({
         root: 'dist',
         livereload:true

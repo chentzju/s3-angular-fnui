9f5fe462e442bed99429a27d85948d62b963f7d1
陈涛 <chent.zju@gmail.com>
2017-02-07 15:30:16 +0800

    增加模块

--- src/app/views/order/js/order-dire.js
+++ src/app/views/order/js/order-dire.js
@@ -1,13 +1,3 @@
 /**
  * Created by chent on 2017/1/18.
  */
-myApp.directive("dropDown",function(){
-    return {
-        restrict:'E',
-        transclude:true,
-        template:'',
-        link:function(scope,element,attr){
-            $(element).dropdown();
-        }
-    }
-});

c064e2c1c0f7b09df9db4e964cc6b76bd6ee4750
chentaozju <18716003620@163.com>
2017-02-08 11:10:30 +0800

    修改payment/payment-ctrl.js   payment-serv.js

--- src/app/views/payment/js/payment-ctrl.js
+++ src/app/views/payment/js/payment-ctrl.js
@@ -1,19 +1,7 @@
-angular.module("myApp").controller("PaymentListCtrl",["$scope","$rootScope","PaymentService",function ($scope,$rootScope,ProductService) {
-    ////$rootScope  在购买的时候应该会有用吧
-    //$scope.products = ProductService.getProductList();
-    //
-    //$scope.refreshPage = function () {
-    //    console.log('刷新');
-    //};
-    //
-    //$scope.loadMore = function () {
-    //    console.log('加载更多产品');
-    //}
+angular.module("myApp").controller("PaymentListCtrl",["$scope","$rootScope","PaymentService",function ($scope,$rootScope,PaymentService) {
+
 }]);
 
-myApp.controller("PaymentListCtrl",["$scope","$rootScope",'$stateParams','PaymentService',function ($scope,$rootScope,$stateParams,ProductService) {
-    //取得传过来的参数
-    //var productId = $stateParams.productId;
-    //// console.log();
-    //$scope.product = ProductService.getProductDetail(productId);
+myApp.controller("PaymentListCtrl",["$scope","$rootScope",'$stateParams','PaymentService',function ($scope,$rootScope,$stateParams,PaymentService) {
+
 }]);
--- src/app/views/payment/js/payment-serv.js
+++ src/app/views/payment/js/payment-serv.js
@@ -1,3 +1,4 @@
-/**
- * Created by Administrator on 2017/2/8 0008.
- */
+angular.module("myApp").service("PaymentService",function(){
+
+
+});

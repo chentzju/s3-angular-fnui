986d528bedaec559f88e6a553daacfe3b752a728
陈涛 <chent.zju@gmail.com>
2017-02-09 15:55:31 +0800

    增加了iscroll内容的响应式高度 目前设置的响应式高度为clientHeight-150px (头、脚和搜索框)

--- src/app/views/account/js/account-ctrl.js
+++ src/app/views/account/js/account-ctrl.js
@@ -4,24 +4,23 @@
 angular.module('myApp').controller('LoginCtrl',['$scope','$rootScope','$state','$es',function ($scope,$rootScope,$state,$es) {
 
 
-    $("#login-form").validator({
-        onValid:function(){
+   // $("#login-form").validator({
+     //   onValid:function(){
             $scope.doLogin = function(){
                 //处理登陆事宜
-                if($scope.loginName === "testuser" && $scope.password === "123456"){
+                //if($scope.loginName === "testuser" && $scope.password === "123456"){
                     $es.userinfo = {userName:'测试名称',unionId:'3817',customerId:'01.001',customerName:'测试公司'};
                     event.preventDefault();
                     $state.go('app');
-                }else{
-                    $scope.errorMessage = "用户名或密码错误";
+               // }else{
+               //     $scope.errorMessage = "用户名或密码错误";
                     $scope.showError = true;
-                }
+               // }
             }
-        },
+      //  },
+      //  onInValid:function(){
+       //     return false;
+     //   }
 
-        onInValid:function(){
-            return false;
-        }
-
-    });
+   // });
 }]);

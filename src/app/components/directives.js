179a71b64997e28ba812a48874ce0809899e1594
陈涛 <chent.zju@gmail.com>
2017-02-17 14:48:59 +0800

    content-scroll更名为scroll-content  去掉replace 修复容器内容超出后无法滚动的bug

--- src/app/components/directives.js
+++ src/app/components/directives.js
@@ -201,15 +201,14 @@ myApp.directive('butterToast', function() {
             }
         }
     })
-    .directive('contentScroll',function(){
+    .directive('scrollContent',function(){
         return{
             restrict:'E',
-            replace:true,
             transclude:true,
             template:'<div class="content-scroll"><div class="content-scroller" ng-transclude></div></div>',
             link:function(scope,element,attr){
                 $(function(){
-                    var scroll = element[0];
+                    var scroll = $(element).find('.content-scroll')[0];
                     new IScroll(scroll,{click:true});
                     scroll.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
                 })
@@ -248,10 +247,7 @@ myApp.directive('butterToast', function() {
         +' <div class="fn-modal-footer">'
         + '<span class="fn-modal-btn fn-modal-btn-cancel" data-fn-modal-cancel>取消</span>'
         + '<span class="fn-modal-btn fn-modal-btn-bold"  data-fn-modal-confirm>确定</span>'
-        +'</div></div></div>',
-        link:function () {
-            modal.confirm('标题','你确定吗？');
-        }
+        +'</div></div></div>'
     }
 })
     /**

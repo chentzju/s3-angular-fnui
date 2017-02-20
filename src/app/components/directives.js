f7325b09277e41a60eb1f2b5d449c47f7a5700af
fanpeiyao <952568789@qq.com>
2017-02-20 09:26:59 +0800

    】

--- src/app/components/directives.js
+++ src/app/components/directives.js
@@ -64,7 +64,7 @@ myApp.directive('butterToast', function() {
                 // });
             }
         }
-    }])
+    })
     .directive("dropDown",function(){
         return {
             restrict:'E',
